e71c54f4377726be1e492dc69509899fb2badb9e
brucejohnson <brucejohnson@fe80:0:0:0:7e7a:19c3:8da9:95e1%11>
2017-04-18 07:26:34 -0400

    Setup getters/setters and init stroke and fill

--- src/main/java/org/jfxplot/RGraphicsContext.java
+++ src/main/java/org/jfxplot/RGraphicsContext.java
@@ -28,52 +28,52 @@ import javafx.scene.text.Font;
 public class RGraphicsContext {
 
     /* pen colour (lines, text, borders, ...) original int*/
-    Color stroke;
+    private Color stroke = Color.BLUE;
 
     /* fill colour (for polygons, circles, rects, ...) original int*/
-    Color fill;
+    private Color fill = Color.YELLOW;
 
     /* Gamma correction */
-    double gamma;
+    private double gamma;
 
     /*
      * Line characteristics
      */
 
  /* Line width (roughly number of pixels) */
-    double lwd;
+    private double lwd;
     /* Line type (solid, dashed, dotted, ...) */
-    int lty;
+    private int lty;
 
     /* line join original enum*/
-    StrokeLineJoin lineJoin = StrokeLineJoin.MITER;
+    private StrokeLineJoin lineJoin = StrokeLineJoin.MITER;
 
     /* Line end original enum*/
-    StrokeLineCap lineCap = StrokeLineCap.BUTT;
+    private StrokeLineCap lineCap = StrokeLineCap.BUTT;
 
     /* line mitre */
-    double lmitre;
+    private double lmitre;
 
-    Font font = Font.getDefault();
+    private Font font = Font.getDefault();
     /*
      * Text characteristics
      */
 
  /* Character expansion (font size = fontsize*cex) */
-    double cex;
+    private double cex;
 
     /* Font size in points */
-    double ps;
+    private double ps;
 
     /* Line height (multiply by font size) */
-    double lineheight;
+    private double lineheight;
 
 
     /* Font face (plain, italic, bold, ...) */
-    int fontface;
+    private int fontface;
 
     /* Font family  original char array*/
-    String fontFamily;
+    private String fontFamily;
 
     public Color getFill() {
         return fill;
@@ -85,6 +85,181 @@ public class RGraphicsContext {
 
     public Font getFont() {
         return font;
+    }
+
+    /**
+     * @param stroke the stroke to set
+     */
+    public void setStroke(Color stroke) {
+        this.stroke = stroke;
+    }
+
+    /**
+     * @param fill the fill to set
+     */
+    public void setFill(Color fill) {
+        this.fill = fill;
+    }
+
+    /**
+     * @return the gamma
+     */
+    public double getGamma() {
+        return gamma;
+    }
+
+    /**
+     * @param gamma the gamma to set
+     */
+    public void setGamma(double gamma) {
+        this.gamma = gamma;
+    }
+
+    /**
+     * @return the lwd
+     */
+    public double getLwd() {
+        return lwd;
+    }
+
+    /**
+     * @param lwd the lwd to set
+     */
+    public void setLwd(double lwd) {
+        this.lwd = lwd;
+    }
+
+    /**
+     * @return the lty
+     */
+    public int getLty() {
+        return lty;
+    }
+
+    /**
+     * @param lty the lty to set
+     */
+    public void setLty(int lty) {
+        this.lty = lty;
+    }
+
+    /**
+     * @return the lineJoin
+     */
+    public StrokeLineJoin getLineJoin() {
+        return lineJoin;
+    }
+
+    /**
+     * @param lineJoin the lineJoin to set
+     */
+    public void setLineJoin(StrokeLineJoin lineJoin) {
+        this.lineJoin = lineJoin;
+    }
+
+    /**
+     * @return the lineCap
+     */
+    public StrokeLineCap getLineCap() {
+        return lineCap;
+    }
+
+    /**
+     * @param lineCap the lineCap to set
+     */
+    public void setLineCap(StrokeLineCap lineCap) {
+        this.lineCap = lineCap;
+    }
+
+    /**
+     * @return the lmitre
+     */
+    public double getLmitre() {
+        return lmitre;
+    }
+
+    /**
+     * @param lmitre the lmitre to set
+     */
+    public void setLmitre(double lmitre) {
+        this.lmitre = lmitre;
+    }
+
+    /**
+     * @param font the font to set
+     */
+    public void setFont(Font font) {
+        this.font = font;
+    }
+
+    /**
+     * @return the cex
+     */
+    public double getCex() {
+        return cex;
+    }
+
+    /**
+     * @param cex the cex to set
+     */
+    public void setCex(double cex) {
+        this.cex = cex;
+    }
+
+    /**
+     * @return the ps
+     */
+    public double getPs() {
+        return ps;
+    }
+
+    /**
+     * @param ps the ps to set
+     */
+    public void setPs(double ps) {
+        this.ps = ps;
+    }
+
+    /**
+     * @return the lineheight
+     */
+    public double getLineheight() {
+        return lineheight;
+    }
+
+    /**
+     * @param lineheight the lineheight to set
+     */
+    public void setLineheight(double lineheight) {
+        this.lineheight = lineheight;
+    }
+
+    /**
+     * @return the fontface
+     */
+    public int getFontface() {
+        return fontface;
+    }
+
+    /**
+     * @param fontface the fontface to set
+     */
+    public void setFontface(int fontface) {
+        this.fontface = fontface;
+    }
+
+    /**
+     * @return the fontFamily
+     */
+    public String getFontFamily() {
+        return fontFamily;
+    }
+
+    /**
+     * @param fontFamily the fontFamily to set
+     */
+    public void setFontFamily(String fontFamily) {
+        this.fontFamily = fontFamily;
     }
 
 }
